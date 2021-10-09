21b7be761346c103334386ef45b182672e5ee956
Akachi <anabantiakachi1@gmail.com>
2021-10-09 20:27:00 +0100

    upgrade the web view and added a form

--- iface.py
+++ iface.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap 
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap=Bootstrap(app)
 
+class FileForm(FlaskForm):
+	data=FileField("uplaod csv", validators=[DataRequired()])
+	submit=SubmitField("upload")
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	return render_template('base.html')
+	return render_template('index.html')
+
+
+@app.route('/import', methods=['GET','POST'])
+def load():
+	form=FileForm()
+	return render_template('import.html', form=form)
