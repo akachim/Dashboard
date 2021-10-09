b1889062c2a3addbb2b63102cae217714f2ecd7e
Akachi <anabantiakachi1@gmail.com>
2021-10-09 20:51:46 +0100

    renamed the import.html to load.html

--- iface.py
+++ iface.py
@@ -5,19 +5,28 @@ from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from wtforms.validators import DataRequired
 
+#---------configurations--------------
+
 app = Flask(__name__)
+app.config["SECRET_KEY"]="oh well i hope it works this time"
+BOOTSTRAP_SERVE_LOCAL=True
+#-------------initializations-----------------------
 bootstrap=Bootstrap(app)
 
+#--------------forms---------------------------
 class FileForm(FlaskForm):
 	data=FileField("uplaod csv", validators=[DataRequired()])
 	submit=SubmitField("upload")
+
+
+#---------views/routes---------------------
 
 @app.route('/', methods=['GET','POST'])
 def index():
 	return render_template('index.html')
 
 
-@app.route('/import', methods=['GET','POST'])
+@app.route('/load', methods=['GET','POST'])
 def load():
 	form=FileForm()
-	return render_template('import.html', form=form)
+	return render_template('load.html', form=form)
