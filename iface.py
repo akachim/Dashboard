d568cfb8f5dcc3fbe7069df83e94634c84b38b18
Akachi <anabantiakachi1@gmail.com>
2021-10-10 05:02:58 +0100

    added a predict route that plots graph

--- iface.py
+++ iface.py
@@ -6,6 +6,11 @@ from wtforms import FileField, SubmitField
 from wtforms.validators import DataRequired
 from flask_modals import Modal
 from flask_modals import render_template_modal
+import csv
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 #---------configurations--------------
 
@@ -28,7 +33,26 @@ class FileForm(FlaskForm):
 @app.route('/', methods=['GET','POST'])
 def index():
 	form = FileForm()
-	modal = 'load-modal'
 	if form.validate_on_submit():
 		flash('You have logged in successfully!', 'success')
-	return render_template_modal('index.html', form=form, modal=modal)
+		redirect(url_for('predict'))
+	return render_template_modal('index.html', form=form, modal='modal-form')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+	if request.method == 'POST':
+		# Create variable for uploaded file
+		f = request.files['fileupload']
+		#store the file contents as a string
+		fstring = f.read()
+		#create list of dictionaries keyed by header row
+		csv_dicts = [{k: v for k, v in row.items()} for row in csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
+	df = pd.DataFrame({
+		"Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+		"Amount": [4, 1, 2, 2, 4, 5],
+		"City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]})
+	fig = px.bar(df, x="Fruit", y="Amount", color="City",    barmode="group")
+	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+	
+	return render_template("results.html", graphJSON=graphJSON)
