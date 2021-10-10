eb11534e1208c4f742a4cd5809e7da2af9579d02
Akachi <anabantiakachi1@gmail.com>
2021-10-10 05:48:38 +0100

    testing the plot function

--- iface.py
+++ iface.py
@@ -24,7 +24,7 @@ modal=Modal(app)
 BOOTSTRAP_SERVE_LOCAL=True
 #--------------forms---------------------------
 class FileForm(FlaskForm):
-	data=FileField("uplaod csv", validators=[DataRequired()])
+	data=FileField(validators=[DataRequired()])
 	submit=SubmitField("Submit")
 
 
@@ -33,26 +33,20 @@ class FileForm(FlaskForm):
 @app.route('/', methods=['GET','POST'])
 def index():
 	form = FileForm()
-	if form.validate_on_submit():
-		flash('You have logged in successfully!', 'success')
-		redirect(url_for('predict'))
-	return render_template_modal('index.html', form=form, modal='modal-form')
-
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
 	if request.method == 'POST':
-		# Create variable for uploaded file
-		f = request.files['fileupload']
-		#store the file contents as a string
-		fstring = f.read()
-		#create list of dictionaries keyed by header row
-		csv_dicts = [{k: v for k, v in row.items()} for row in csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
-	df = pd.DataFrame({
-		"Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-		"Amount": [4, 1, 2, 2, 4, 5],
-		"City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]})
-	fig = px.bar(df, x="Fruit", y="Amount", color="City",    barmode="group")
-	graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-	
-	return render_template("results.html", graphJSON=graphJSON)
+		if form.validate_on_submit():
+			try:
+				f = request.files['fileupload']
+				#store the file contents as a string
+				fstring = f.read()
+				#create list of dictionaries keyed by header row
+				csv_dicts = [{k: v for k, v in row.items()} for row in csv.DictReader(fstring.splitlines(), skipinitialspace=True)]
+				df = pd.DataFrame(csv_dicts)
+				fig = px.bar(df, x="Fruit", y="Amount", color="City",    barmode="group")
+				graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+				return render_template_modal('results.html',
+                                form=form, graphJSON=graphJSON, modal=None)
+			except FileNotFoundError as e:
+				return e
+
+	return render_template_modal('index.html', form=form, modal='modal-form')
